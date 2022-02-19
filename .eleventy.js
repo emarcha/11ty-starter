3ea8067761ceea4be5314f0e55c0ed65dfc5f09b
Emmanuel Marcha <info@emmanuelmarcha.com>
2022-02-18 23:14:17 -0600

    Add sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const eleventySass = require("@grimlink/eleventy-plugin-sass");
+const sass = require("sass");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass, { sass });
+
   return {
     dir: {
       input: "src",
